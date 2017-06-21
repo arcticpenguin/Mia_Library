23ab0a8650a04721564abc5d4dcd96063c6f88e5
Mia Feng <mi.feng@pnnl.gov>
2017-06-21 13:58:55 -0700

    javascript template - basic

---
+++ Javascript/project template - basic/js/script.js
@@ -0,0 +1,19 @@
+var data = ["Option 1", "Option 2", "Option 3"];
+
+var select = d3.select('body')
+  .append('select')
+  	.attr('class','select')
+    .on('change',onchange)
+
+var options = select
+  .selectAll('option')
+	.data(data).enter()
+	.append('option')
+		.text(function (d) { return d; });
+
+function onchange() {
+	selectValue = d3.select('select').property('value')
+	d3.select('body')
+		.append('p')
+		.text(selectValue + ' is the last selected option.')
+};
